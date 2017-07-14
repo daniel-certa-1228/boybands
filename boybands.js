6ab8baca5f8e46313db30984bd4db19222c8a604
daniel-certa-1228 <mfdancerta@gmail.com>
2017-07-14 09:33:04 -0500

    finish exercise

--- boybands.js
+++ boybands.js
@@ -4,7 +4,8 @@ var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
 // The number of loops to perform (what if the array changes?)
-var loopCount = 5;
+var loopCount = bands.length;
+console.log("loopCount", loopCount)
 
 // Keep track of which band we're on in the loop
 var currentBand = "";
@@ -13,19 +14,26 @@ var currentBand = "";
 var currentVeggie = "";
 
 // Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById(???);
+var bandElement = document.getElementById("boy-bands");
+console.log("bandElement", bandElement);
 
 // Get a reference to the appropriate DOM element for vegetables
-var veggieElement = ???;
+var veggieElement = document.getElementById("vegetables");
+console.log("veggieElement", veggieElement);
 
 // Start looping
 for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
   
   // Add the band names into the correct <div>
-  currentBand = ???;
+  currentBand = bands[loopTracker];
+  console.log("currentBand", currentBand);
+  let bandPara = `<p>${currentBand}</p>`;
+  bandElement.innerHTML += bandPara;
 
   
   // Add the veggie names into the correct <div>
-  currentVeggie = ???;
-
+  currentVeggie = vegetables[loopTracker];
+  console.log("currentVeggie", currentVeggie);
+  let veggiePara = `<p>${currentVeggie}</p>`;
+  veggieElement.innerHTML += veggiePara;
 }
